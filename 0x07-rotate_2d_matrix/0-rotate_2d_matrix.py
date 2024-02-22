d349a09138cb947f59004a5897cdbc4762009f88
konditi1 <konditi1@gmail.com>
2024-02-23 00:21:00 +0300

    pycodestyle correction

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -9,10 +9,10 @@ def rotate_2d_matrix(matrix):
     Rotates a given 2D matrix 90 degrees clockwise in-place.
 
     Args:
-    - matrix (list of lists): The 2D matrix to be rotated. Must be a square matrix.
+    - matrix (list of lists): 2D matrix to be rotated. Must be a square matrix.
 
     Returns:
-    - None: The function modifies the input matrix in-place and does not return anything.
+    - None: func modifies input matrix in-place and does not return anything.
     """
 
     n = len(matrix)
